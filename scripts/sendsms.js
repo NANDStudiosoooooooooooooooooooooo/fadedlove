e1f5dbe1c108d2f323fa9f7d5146cde63204735c
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-09-27 21:12:38 +0200

    up

--- scripts/sendsms.js
+++ scripts/sendsms.js
@@ -1,38 +1,35 @@
 document.getElementById('smsForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Verhindert das standardmäßige Formular-Submit-Verhalten
 
-    const phoneNumber = document.getElementById('phoneNumber').value;
-const apiToken = '121ca29ada0c465bba944a89dc0d1936'; // Ersetze dies durch deinen API Token
-const servicePlanId = 'a86db25ed99d4f5ea75dafb1b6995ba5'; // Ersetze dies durch deine Service Plan ID
-const senderNumber = '+447520662455'; // Deine Sendernummer
-const apiUrl = `https://us.sms.api.sinch.com/xms/v1/${servicePlanId}/messages`;
+    const phoneNumber = document.getElementById('phoneNumber').value; // Empfängernummer
+    const data = {
+        from: '+447520662455', // Deine Sendernummer
+        to: phoneNumber, // Empfängernummer
+        body: 'Thank you for subscribing to FADEDLOVE SMS Newsletter!' // Nachricht
+    };
 
-// Beispiel für den SMS-Inhalt
-const data = {
-    from: senderNumber, // Sendernummer hier verwenden
-    to: phoneNumber, // Empfängernummer
-    body: 'THANKS FOR SUBSCRIBING TO THE FADEDCLOTH SMS NEWSLETTER!' // Nachricht
-};
+    // URL deines Cloudflare Workers
+    const apiUrl = 'https://sendfadedsms.nand339.workers.dev/'; // Ersetze dies durch die URL deines Workers
 
-// Funktion zum Senden der SMS
-fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiToken}`
-    },
-    body: JSON.stringify(data)
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Error sending SMS');
-    }
-    return response.json();
-})
-.then(data => {
-    console.log('SMS sent successfully:', data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error sending SMS');
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById('smsMessage').innerText = 'Successfully subscribed!';
+        document.getElementById('smsForm').reset(); // Formular zurücksetzen
+    })
+    .catch(error => {
+        document.getElementById('smsMessage').innerText = 'Error: ' + error.message;
+        console.error('Error:', error);
+    });
 });
