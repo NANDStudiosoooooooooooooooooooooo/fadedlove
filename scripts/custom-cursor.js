443887eb9defbdb0cd975dc56c9f57553f8983ea
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-09-28 15:45:27 +0200

    up

--- scripts/custom-cursor.js
+++ scripts/custom-cursor.js
@@ -1,20 +1,26 @@
-const cursor = document.createElement('div');
-        cursor.classList.add('custom-cursor');
-        document.body.appendChild(cursor);
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
 
-        // Update Cursor-Position bei Mausbewegung
-        document.addEventListener('mousemove', (e) => {
-            cursor.style.left = `${e.pageX}px`;
-            cursor.style.top = `${e.pageY}px`;
-        });
+        // Wenn es kein Mobilgerät ist, erstelle den benutzerdefinierten Cursor
+        if (!isMobile) {
+            // Cursor-Element erstellen
+            const cursor = document.createElement('div');
+            cursor.classList.add('custom-cursor');
+            document.body.appendChild(cursor);
 
-        // Hover-Effekt bei Links und Buttons
-        const hoverElements = document.querySelectorAll('a, button');
-        hoverElements.forEach(element => {
-            element.addEventListener('mouseenter', () => {
-                cursor.classList.add('hover'); // Klasse hinzufügen, wenn man über Link/Button fährt
+            // Update Cursor-Position bei Mausbewegung
+            document.addEventListener('mousemove', (e) => {
+                cursor.style.left = `${e.pageX}px`;
+                cursor.style.top = `${e.pageY}px`;
             });
-            element.addEventListener('mouseleave', () => {
-                cursor.classList.remove('hover'); // Klasse entfernen, wenn man den Link/Button verlässt
+
+            // Hover-Effekt bei Links und Buttons
+            const hoverElements = document.querySelectorAll('a, button');
+            hoverElements.forEach(element => {
+                element.addEventListener('mouseenter', () => {
+                    cursor.classList.add('hover'); // Klasse hinzufügen, wenn man über Link/Button fährt
+                });
+                element.addEventListener('mouseleave', () => {
+                    cursor.classList.remove('hover'); // Klasse entfernen, wenn man den Link/Button verlässt
+                });
             });
-        });
+        }
