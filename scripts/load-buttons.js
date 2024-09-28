7859b0858aee052363b290d3f51af76d8e8fd94a
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-09-28 13:20:28 +0200

    up

--- scripts/load-buttons.js
+++ scripts/load-buttons.js
@@ -1,8 +1,18 @@
-
 fetch('buttons.html')
-.then(response => response.text())
-.then(data => {
-    document.body.insertAdjacentHTML('beforeend', data);
-})
-.catch(error => console.error('Error loading buttons:', error));
+            .then(response => response.text())
+            .then(data => {
+                document.body.insertAdjacentHTML('beforeend', data);
+                // Hier kannst du auch die Event Listener hinzufügen
+                addButtonEventListeners(); // Funktion um Event Listener hinzuzufügen
+            })
+            .catch(error => console.error('Error loading buttons:', error));
 
+        function addButtonEventListeners() {
+            const buttons = document.querySelectorAll('.glass-button');
+            buttons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const targetPanel = this.getAttribute('data-target');
+                    togglePanel(targetPanel);
+                });
+            });
+        }
