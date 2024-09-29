1c8e8bf505ab397b121f3b2b20b997484739b4c9
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-09-29 13:38:38 +0200

    Add files via upload

--- scripts/legal2.js
+++ scripts/legal2.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Funktion zum Laden der Markdown-Datei
     function loadMarkdown(contentId) {
-        const filePath = `/legal/${contentId}.md`; // Pfad zu den Markdown-Dateien
+        const filePath = `/text/${contentId}.md`; // Pfad zu den Markdown-Dateien
         
         fetch(filePath)
             .then(response => {
---
+++ scripts/subscribe.js
@@ -0,0 +1,40 @@
+document.getElementById("subscribeButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const emailInput = document.getElementById("email");
+    const email = emailInput.value;
+
+    // Validate email
+    if (isValidEmail(email)) {
+        fetch("https://subscribe.fadedcloth.de/sub", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: email })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const currentUrl = encodeURIComponent(window.location.href);
+            window.location.href = `https://subscribe.fadedcloth.de/success?SUBSCRIBED&referrer=${currentUrl}`;
+        })
+        .catch(error => {
+            alert("An error occurred: " + error.message);
+        });
+    } else {
+        shakeEmailInput(emailInput);
+    }
+});
+
+// Function to validate email format
+function isValidEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+// Function to shake the email input field
+function shakeEmailInput(input) {
+    input.classList.add("shake");
+    setTimeout(() => {
+        input.classList.remove("shake");
+    }, 500); // Remove shake effect after 500ms
+}
