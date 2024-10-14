a0f88f0ec209f104fc16cb2c695c1c36b7f82a0a
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-10-14 21:32:00 +0200

    up

--- scripts/item.js
+++ scripts/item.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Produktdaten abrufen
     client.product.fetch(itemId).then((product) => {
-        displayItem(product);
+        
 
         // Die Produkt-GID (Global ID) für GraphQL anpassen (id muss in base64-Format konvertiert werden)
         const productGID = btoa(`gid://shopify/Product/${itemId}`);
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         client.graphQLClient.send({
             query: `
                 query {
-                    product(id: "${productGID}") {
+                    product(id: "${itemId}") {
                         metafields(first: 10) {
                             edges {
                                 node {
@@ -98,6 +98,8 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error("Error fetching Shopify product:", error);
         document.getElementById('itemDetails').innerHTML = '<p>Failed to fetch product details.</p>';
     });
+
+    displayItem(product);
 
     // Funktion zum Anzeigen des Produkts
     function displayItem(product) {
