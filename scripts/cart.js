f3576bfd2a1878e1affc17a7877880484b8f744e
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-10-23 16:37:27 +0200

    up

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,3 @@
-// Shopify Client initialisieren
 const client = ShopifyBuy.buildClient({
     domain: 'checkout.fadedcloth.de',
     storefrontAccessToken: 'ed72f09d8742f37356305b6e49310909'
@@ -6,36 +5,31 @@ const client = ShopifyBuy.buildClient({
 
 let checkoutId = localStorage.getItem('checkoutId');
 console.log(checkoutId);
-
 if (!checkoutId) {
     // Erstelle neuen Checkout, falls keiner existiert
     client.checkout.create().then((checkout) => {
         checkoutId = checkout.id;
         localStorage.setItem('checkoutId', checkoutId);
-        console.log('Neuer Checkout in  cart erstellt:', checkoutId);
+        console.log('Neuer Checkout in cart erstellt:', checkoutId);
     });
 } else {
     // Benutze vorhandenen Checkout
-    console.log('Vorhandener VON CART GELADEN Checkout:', checkoutId);
+    console.log('Vorhandener Checkout geladen:', checkoutId);
 }
 
-    const cartButtonContainer = document.getElementById('cart-button-container');
-    cartButtonContainer.innerHTML = ''; // Vorhandene Buttons leeren
-
-    const cartButton = document.createElement('button');
-    cartButton.innerText = 'CART';
-    cartButton.id = 'cart-button';
-    cartButton.classList.add('glass-button');
-    cartButton.addEventListener('click', function () {
-        let NewCheckoutId = localStorage.getItem('checkoutId');
-        if (NewCheckoutId) {
-            window.location.href = `https://checkout.fadedcloth.de/cart?id=${checkoutId}`;
-            console.log(`https://checkout.fadedcloth.de/cart?id=${checkoutId}`);
-        } else {
-            console.error('Checkout ID fehlt.');
-        }
-    });
-
-    cartButtonContainer.appendChild(cartButton);
-
-
+const cartButtonContainer = document.getElementById('cart-button-container');
+cartButtonContainer.innerHTML = ''; // Vorhandene Buttons leeren
+const cartButton = document.createElement('button');
+cartButton.innerText = 'CART';
+cartButton.id = 'cart-button';
+cartButton.classList.add('glass-button');
+cartButton.addEventListener('click', function () {
+    let NewCheckoutId = localStorage.getItem('checkoutId');
+    if (NewCheckoutId) {
+        window.location.href = `https://checkout.fadedcloth.de/cart?id=${NewCheckoutId}`;
+        console.log(`https://checkout.fadedcloth.de/cart?id=${NewCheckoutId}`);
+    } else {
+        console.error('Checkout ID fehlt.');
+    }
+});
+cartButtonContainer.appendChild(cartButton);
