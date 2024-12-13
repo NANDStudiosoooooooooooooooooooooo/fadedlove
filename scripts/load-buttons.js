701431c79a59b107b14014117960a066472641d5
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2024-12-13 19:50:18 +0100

    up

--- scripts/load-buttons.js
+++ scripts/load-buttons.js
@@ -180,7 +180,7 @@ function makeMovable() {
     movableDivs.forEach(function(movableDiv) {
         let isDragging = false;
         let offsetX = 0, offsetY = 0;
-
+s
         // Für Desktop (PC) - Mausereignisse
         movableDiv.addEventListener('mousedown', function(e) {
             if (window.innerWidth >= 768) { // Nur auf PC und Tablets, aber nicht auf mobilen Geräten
