e1708f5690311feccf3de5443df73694abc2f13b
nand studios <139035548+NANDStudiosoooooooooooooooooooooo@users.noreply.github.com>
2025-02-10 16:25:32 +0100

    Added Webpack configuration and fixed CSS issues

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,38 @@ document.addEventListener("DOMContentLoaded", function() {
     animateElements('top', 'slideInTop');
     animateElements('top-right', 'slideInRight');
 });
+
+document.getElementById("subscribeButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    const emailInput = document.getElementById("email");
+    const email = emailInput.value;
+    const termsCheckbox = document.getElementById("termsCheckbox");
+    const checkboxContainer = document.querySelector('.checkbox-container');
+
+    // Zuerst die E-Mail validieren
+    if (!isValidEmail(email)) {
+        shakeElement(emailInput); // Nur das E-Mail-Feld wackelt
+    } else {
+        // E-Mail ist gültig, dann prüfen, ob die Checkbox angeklickt ist
+        if (!termsCheckbox.checked) {
+            shakeElement(checkboxContainer); // Jetzt nur die Checkbox und der Text wackeln
+        } else {
+            // Wenn beides korrekt ist, den normalen Submit-Prozess starten
+            fetch("https://subscribe.fadedcloth.de/sub", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: email })
+            })
+            .then(response => response.json())
+            .then(data => {
+                const currentUrl = encodeURIComponent(window.location.href);
+                window.location.href = `https://subscribe.fadedcloth.de/success?SUBSCRIBED&referrer=${currentUrl}`;
+            })
+            .catch(error => {
+                alert("An error occurred: " + error.message);
+            });
+        }
+    }
+});
